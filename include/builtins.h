1dfa1190485cd9e7cf58294ee88946c5561c0884
zeviberlin <zeviberlin@gmail.com>
2026-07-07 19:16:59 -0400

    creating built in functions for the macro system

---
+++ include/builtins.h
@@ -0,0 +1,41 @@
+#ifndef BUILTINS_H
+#define BUILTINS_H
+
+#if defined(__APPLE__) || defined(__MACH__)
+  #include "macos/mouse/mouse.h"
+#elif defined(__LINUX__) || defined(__unix__)
+  #include "linux/mouse/mouse.h"
+#endif
+
+
+/// built-in function for left clicking at a certain position
+static leftClick(int x = -1, int y = -1) {
+  if (x >= 0 && y >= 0) move(x, y);
+
+  clickMouse(LEFT, LEFT_DOWN);
+  clickMouse(LEFT, LEFT_UP);
+}
+
+/// built-in function for right clicking at a certain position
+static rightClick(int x = -1, int y = -1) {
+  if (x >= 0 && y >= 0) move(x, y);
+
+  clickMouse(RIGHT, RIGHT_DOWN);
+  clickMouse(RIGHT, RIGHT_UP);
+}
+
+//
+
+/// built-in function for double left clicking at a certain position
+static leftDoubleClick(int x = -1, int y = -1) {
+  leftClick(x, y);
+  leftClick(x, y);
+}
+
+/// built-in function for double right clicking at a certain position
+static rightDoubleClick(int x = -1, int y = -1) {
+  rightClick(x, y);
+  rightClick(x, y);
+}
+
+#endif /* BUILTINS_H */
