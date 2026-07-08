ff33a05d7dbe90cb2cc0582772b731236d5cce13
zeviberlin <zeviberlin@gmail.com>
2026-07-07 20:09:47 -0400

    idk why these werent included

---
+++ include/macros.h
@@ -0,0 +1,40 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+#if defined(__APPLE__) || defined(__MACH__)
+  #include "macos/mouse/mouse.h"
+#elif defined(__LINUX__) || defined(__unix__)
+  #include "linux/mouse/mouse.h"
+#endif
+
+#include <ApplicationServices/ApplicationServices.h> // MacOS Quartz Event Services
+
+#include <unistd.h>
+
+#include "builtins.h"
+
+typedef struct macro_part {
+  int x;
+  int y;
+  
+  void (*func)(int x, int y);
+  
+  struct macro_part* next;
+} macro_part;
+
+typedef struct Macro {
+  macro_part* first;
+  macro_part* last;
+} Macro;
+
+/// Could return NULL, that check falls on the caller
+Macro* initializeMacro();
+
+void freeMacro(Macro* mac);
+
+void addMacroStep(Macro* mac, void (*func)(int x, int y), int x, int y);
+void removeMacroStep(Macro* mac, int offset);
+
+void runMacro(const Macro* mac);
+
+#endif /* MACROS_H */
---
+++ src/macros.c
@@ -0,0 +1,67 @@
+#include "macros.h"
+
+#include <ApplicationServices/ApplicationServices.h> // MacOS Quartz Event Services
+
+#include <unistd.h>
+
+#include "builtins.h"
+
+
+Macro* initializeMacro() {
+  Macro* mac = malloc(sizeof(*mac));
+  if (!mac) return NULL;
+
+  mac->first = NULL;
+  mac->last = NULL;
+
+  return mac;
+}
+
+
+void freeMacro(Macro* mac) {
+  while (mac->first != mac->last) {
+    macro_part* new_head = mac->first->next;
+    free(mac->first);
+    mac->first = new_head;
+  }
+
+  free(mac->last);
+  free(mac);
+}
+
+
+void addMacroStep(Macro* mac, void (*func)(int x, int y), int x, int y) {
+  macro_part* part = malloc(sizeof(*part));
+  if (!part) return;
+
+  part->func = func;
+  part->x = x;
+  part->y = y;
+
+  if (mac->first == NULL) {
+    part->next = part;
+
+    mac->first = part;
+    mac->last = part;
+  } else {
+    part->next = NULL;
+
+    mac->last->next = part;
+    mac->last = part;
+  }
+}
+
+
+void removeMacroStep(Macro* mac, int offset) {
+  return;
+}
+
+void runMacro(const Macro* mac) {
+  macro_part* part = mac->first;
+  while (part != mac->last) {
+    part->func(part->x, part->y);
+    part = part->next;
+  }
+
+  part->func(part->x, part->y); // this is the last call
+}
