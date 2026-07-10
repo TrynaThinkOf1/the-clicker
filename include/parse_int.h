0853628305b06a6431c42207fc7922eb4a0a45d4
zeviberlin <zeviberlin@gmail.com>
2026-07-10 13:29:08 -0400

    add back negative parsing

--- include/parse_int.h
+++ include/parse_int.h
@@ -6,15 +6,22 @@
 #include <stdbool.h>
 
 
-static int parse_int(const char* str, int len) {
+static int parse_int(char* str, int len) {
   int result = 0;
 
+  bool neg = false;
+  if (*str == '-') {
+    neg = true;
+    str++;
+  }
+  
   int idx = 0;
   for (char* c = str + len; c >= str; c--) {
    	result += (*c - 48) * pow(10, idx);
    	idx++;
   }
 
+  if (neg) result = ~result + 1; // Computer Science: A Programmer's Perspective
   return result;
 }
 
