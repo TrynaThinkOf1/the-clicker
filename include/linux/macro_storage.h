298461d4dcde22a9b0b7a54811e4f4856ea562f1
zeviberlin <zeviberlin@gmail.com>
2026-08-06 18:19:28 -0400

    well underway with some new MUCH cleaner file handling functions using VLA's

--- include/linux/macro_storage.h
+++ include/linux/macro_storage.h
@@ -3,10 +3,56 @@
 
 #if defined(__APPLE__) || defined(__MACH__) || defined(__LINUX__) || defined(__unix__)
 
+  #include <stdio.h>
   #include <pwd.h>
   #include <unistd.h>
+  #include <sys/stat.h>
+  #include <sys/types.h>
+  #include <stdbool.h>
+  #include <string.h>
+
+  #include "macros.h"
+  #include "builtins.h"
 
   char* getHomeDir();
+
+  /*
+   * Can be used for both files and directories
+   */
+  bool filepathExists(const char* abs_name);
+
+  /*
+   * @contract: the passed name does not already exist
+   */
+  bool createDirectory(const char* abs_name);
+
+  /*
+   * Caller creates stack VLA to pass in
+   * Example:
+   *   char* names[3] = {"root", "directory", "file.txt"};
+   *
+   *   int size = 0;
+   *   for (int i = 0; i < 3; i++) size += strlen(names[i]);
+   *
+   *   char abs[size + 3]; // the size of all the names and all the '/'
+   *   createAbsName(names, 3, abs, size + 3); // "root/directory/file.txt" (technically bool true/false)
+   *
+   * @contract: absolute is calculated to be wide enough
+   */
+  bool createAbsName(char** names, int len, char* restrict absolute, int abs_len);
+
+  void deleteFile(const char* abs_name);
+
+  //
+
+  bool exportMacro(Macro* mac, char* error);
+
+  /*
+   * Frees `op_mac` on error
+   */
+  bool importMacro(const char* name, Macro* op_mac, char* error);
+
+  void deleteMacro(Macro* mac);
 
 #endif
 
--- src/unix_macro_storage.c
+++ src/unix_macro_storage.c
@@ -7,4 +7,47 @@
     return info->pw_dir;
   }
 
+  bool directoryExists(const char* abs_name) {
+    struct stat st = {0};
+    return stat(abs_name, &st) != -1;
+  }
+
+  bool createDirectory(const char* abs_name) {
+    // these permissions are READ/WRITE/EXEC for all users and groups, kinda unsafe :/
+    return mkdir(abs_name, S_IRWXU | S_IRWXG | S_IRWXO) != -1;
+  }
+
+  bool createAbsName(const char** names, int len, char* restrict absolute, int abs_len) {
+    absolute[0] = '\0'; // super elusive garbage-init bug causes whole func to fail
+    int offset = 0;
+
+    for (int i = 0; i < len; i++) {
+      register int size = strlen(names[i]); // not actually recomputed, it gets cached
+      if (strlcat(absolute, names[i], abs_len) < size + offset || strlcat(absolute, "/", abs_len) < size + offset + 1) return false;
+      offset += size + 1; // + 1 for the '/'
+    }
+
+    return true;
+  }
+
+  void deleteFile(const char* abs_name) {
+    remove(abs_name);
+  }
+
+  //
+
+  
+  bool exportMacro(Macro* mac, char* error) {
+    mac->saved = true;
+    return true;
+  }
+
+  bool importMacro(const char* name, Macro* op_mac, char* error) {
+    return true;
+  }
+
+  void deleteMacro(Macro* mac) {
+    mac->saved = false;
+  }
+
 #endif
