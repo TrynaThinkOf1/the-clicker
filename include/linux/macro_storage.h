3ea1c050c4950768551d5803a0723459be447118
zeviberlin <zeviberlin@gmail.com>
2026-08-06 16:36:06 -0400

    simplest function ever, stack only as well

--- include/linux/macro_storage.h
+++ include/linux/macro_storage.h
@@ -3,32 +3,10 @@
 
 #if defined(__APPLE__) || defined(__MACH__) || defined(__LINUX__) || defined(__unix__)
 
-  #include <stdlib.h>
-  #include <stdio.h>
-  #include <unistd.h>
-  #include <sys/stat.h>
-  #include <sys/types.h>
   #include <pwd.h>
-  #include <string.h>
-  #include <stdbool.h>
+  #include <unistd.h>
 
-  #include "macros.h"
-  #include "builtins.h"
-  #include "parse_int.h"
-
-  #define MACRO_STORAGE "/.clicker_macros/"
-
-
-  bool directoryExists(const char* dir);
-  bool createDirectory(const char* dir);
-
-  bool nameExists(const char* name);
-
-  bool exportMacro(Macro* mac);
-
-  Macro* importMacro(char* name);
-
-  void deleteMacro(const char* name);
+  char* getHomeDir();
 
 #endif
 
