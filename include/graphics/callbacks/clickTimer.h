36652b554ead67fbbcb21e8d85261bcc8c0dd499
zeviberlin <zeviberlin@gmail.com>
2026-08-24 20:34:39 -0400

    make sure not to have incompatible UB types

--- include/graphics/callbacks/clickTimer.h
+++ include/graphics/callbacks/clickTimer.h
@@ -6,13 +6,15 @@
 #include "graphics/state.h"
 
 
-static void clickTimer(const ClickTimerState* state) {
+static gpointer clickTimer(gpointer user_data) {
+  ClickTimerState* state = (ClickTimerState*)user_data;
+  
   while (g_atomic_int_get(&state->timer_active)) {
     g_usleep(state->sleep_ms * 1000);
     state->click_func(state->x, state->y);
   }
   
-  return;
+  return NULL;
 }
 
 #endif /* CLICKTIMER_H */
