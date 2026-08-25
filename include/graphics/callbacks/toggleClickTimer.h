4856ad8051d02f0a2951c3a1217b40b879db7a60
zeviberlin <zeviberlin@gmail.com>
2026-08-24 20:34:48 -0400

    seems to be working just fine!

--- include/graphics/callbacks/toggleClickTimer.h
+++ include/graphics/callbacks/toggleClickTimer.h
@@ -3,39 +3,57 @@
 
 #include <gtk/gtk.h>
 
+#include "builtins.h"
 #include "graphics/state.h"
 #include "graphics/sendNotification.h"
 #include "linux/macro_storage.h"
-#include "debug_tests.h"
+#include "graphics/callbacks/clickTimer.h"
 
 
 static void toggleClickTimer(GtkWidget* button, gpointer user_data) {
   ClickTimerState* state = (ClickTimerState*)user_data;
 
-  long wait_in_ms =
+  state->sleep_ms =
     (gtk_spin_button_get_value_as_int(state->mins_spinbtn) * 60 * 1000)
     + (gtk_spin_button_get_value_as_int(state->secs_spinbtn) * 1000)
     + gtk_spin_button_get_value_as_int(state->ms_spinbtn);
 
-  if (wait_in_ms <= 0) {
-    // GtkAlertDialog* dialog = gtk_alert_dialog_new("Invalid Wait Time");
-    // gtk_alert_dialog_set_detail(dialog, "Timer must be more than 0ms between clicks.");
-    // gtk_alert_dialog_show(dialog, state->main_window);
-
-    // GNotification* noti = g_notification_new("Invalid Wait Time");
-    // g_notification_set_body(noti, "Timer must be for more than 0ms between clicks.");
-    // g_notification_set_priority(noti, G_NOTIFICATION_PRIORITY_HIGH);
-    
-    // g_application_send_notification(G_APPLICATION(state->app), "click-timer-invalid-wait-time", noti);
-
-    // g_object_unref(noti);
-
+  if (state->sleep_ms <= 0) {
     send_notification("Invalid Wait Time", "Timer must be for more than 0ms between clicks", state->app);
+    return;
   }
 
-  CLICKER_ASSERT(wait_in_ms > 0, "Click Timer Wait is less than or equal to 0ms!");
+  const char* xstr = gtk_entry_buffer_get_text(gtk_entry_get_buffer(state->x_coord_entry));
+  const char* ystr = gtk_entry_buffer_get_text(gtk_entry_get_buffer(state->y_coord_entry));
 
-  // TODO: Implement the thread adder
+  if (strlen(xstr) == 0 || strlen(ystr) == 0) {
+    state->x = -1;
+    state->y = -1;
+  } else {
+    if (sscanf(xstr, "%i", &state->x) != 1) {
+      send_notification("Invalid X Coordinate", "The X coordinate for the click timer must be a number between 0 and 2^31 - 1", state->app);
+      return;
+    }
+    if (sscanf(ystr, "%i", &state->y) != 1) {
+      send_notification("Invalid Y Coordinate", "The Y coordinate for the click timer must be a number between 0 and 2^31 - 1", state->app);
+      return;
+    }
+  }
+
+  // do the threading
+
+  if (!g_atomic_int_get(&state->timer_active)) {
+    g_atomic_int_set(&state->timer_active, TRUE);
+    state->timer_thread = g_thread_new("click-timer", clickTimer, state);
+
+    gtk_button_set_label(GTK_BUTTON(state->start_stop_button), "STOP");
+  } else {
+    g_atomic_int_set(&state->timer_active, FALSE);
+    g_thread_join(state->timer_thread);
+    state->timer_thread = NULL;
+    
+    gtk_button_set_label(GTK_BUTTON(state->start_stop_button), "START");
+  }
 }
 
 #endif /* TOGGLECLICKTIMER_H */
