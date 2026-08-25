a7f1db8ca932ed5c52ddb14796b6bf557dc00de1
zeviberlin <zeviberlin@gmail.com>
2026-08-24 20:24:08 -0400

    deprecate `parse_int()`

--- include/parse_int.h
+++ include/parse_int.h
@@ -1,29 +1,30 @@
-#ifndef PARSE_INT_H
-#define PARSE_INT_H
+/* DEPRECATED - USING SSCANF() NOW */
+// #ifndef PARSE_INT_H
+// #define PARSE_INT_H
 
-#include <math.h>
-#include <string.h>
-#include <stdbool.h>
+// #include <math.h>
+// #include <string.h>
+// #include <stdbool.h>
 
 
-static int parse_int(char* str, int len) {
-  int result = 0;
+// static int parse_int(char* str, int len) {
+//   int result = 0;
 
-  bool neg = false;
-  if (*str == '-') {
-    neg = true;
-    str++;
-  }
+//   bool neg = false;
+//   if (*str == '-') {
+//     neg = true;
+//     str++;
+//   }
   
-  int idx = 0;
-  for (char* c = str + len; c >= str; c--) {
-   	result += (*c - 48) * pow(10, idx);
-   	idx++;
-  }
+//   int idx = 0;
+//   for (char* c = str + len; c >= str; c--) {
+//    	result += (*c - 48) * pow(10, idx);
+//    	idx++;
+//   }
 
-  if (neg) result = ~result + 1; // Computer Science: A Programmer's Perspective
-  return result;
-}
+//   if (neg) result = ~result + 1; // Computer Science: A Programmer's Perspective
+//   return result;
+// }
 
 
-#endif /* PARSE_INT_H */
+// #endif /* PARSE_INT_H */
