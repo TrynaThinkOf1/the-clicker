1b421fe153b8cb4b555656bb6e4f82b38d4872d7
zeviberlin <zeviberlin@gmail.com>
2026-08-28 18:47:00 -0400

    pointer mismatch fixes + preliminary macro loader ui added
    WILL SPLIT INTO TWO FUNCS IN FUTURE

--- include/graphics/activate.h
+++ include/graphics/activate.h
@@ -5,10 +5,11 @@
 #include <stdbool.h>
 
 #include "graphics/state.h"
+#include "graphics/callbacks/macroLoaderInfoDialog.h"
 #include "graphics/callbacks/clickTimerInfoDialog.h"
-#include "graphics/callbacks/loadClickTimerFunc.h"
 #include "graphics/callbacks/toggleClickTimer.h"
 #include "graphics/callbacks/numberOnlyEntry.h"
+#include "gtk/gtkshortcut.h"
 
 static void activate(GtkApplication* app, gpointer user_data) {
 
@@ -16,6 +17,8 @@ static void activate(GtkApplication* app, gpointer user_data) {
   GtkBuilder* main_window_builder = gtk_builder_new_from_resource("/com/the-clicker/graphics/ui/main_window.ui");
   GtkApplicationWindow* window = GTK_APPLICATION_WINDOW(gtk_builder_get_object(main_window_builder, "main_window"));
   gtk_window_set_application(GTK_WINDOW(window), app);
+
+  GtkWidget* container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
   /* */
 
   /* CLICK TIMER
@@ -25,9 +28,9 @@ static void activate(GtkApplication* app, gpointer user_data) {
    * [          START/STOP BUTTON          ]
    * Relies on a global state struct for everything
    */
-  GtkBox* container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-  gtk_widget_add_css_class(GTK_WIDGET(container), "click-timer-box");
-  gtk_widget_set_overflow(container, GTK_OVERFLOW_HIDDEN); // make corners round
+  GtkWidget* click_timer_container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+  gtk_widget_add_css_class(click_timer_container, "click-timer-box");
+  gtk_widget_set_overflow(click_timer_container, GTK_OVERFLOW_HIDDEN); // make corners round
   
   GtkBuilder* click_timer_builder = gtk_builder_new_from_resource("/com/the-clicker/graphics/ui/click_timer.ui");
   ClickTimerState* CT_STATE = g_new0(ClickTimerState, 1); // deliberatly leaked, actual pattern for GTK+
@@ -58,7 +61,7 @@ static void activate(GtkApplication* app, gpointer user_data) {
     gtk_grid_attach(GTK_GRID(label_and_info_grid), click_timer_label, 0, 0, 1, 1);
     gtk_grid_attach(GTK_GRID(label_and_info_grid), gtk_label_new(" "), 1, 0, 1, 1); // spacer
     gtk_grid_attach(GTK_GRID(label_and_info_grid), click_timer_info_button, 2, 0, 1, 1);
-    gtk_box_append(container, label_and_info_grid);
+    gtk_box_append(GTK_BOX(click_timer_container), label_and_info_grid);
 
   GtkWidget* click_timer_grid = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "click_timer_grid"));
   
@@ -81,8 +84,57 @@ static void activate(GtkApplication* app, gpointer user_data) {
   g_signal_connect(CT_STATE->start_stop_button, "clicked", G_CALLBACK(toggleClickTimer), CT_STATE);
   gtk_grid_attach(GTK_GRID(click_timer_grid), CT_STATE->start_stop_button, 0, 2, 3, 1);
 
-  gtk_box_append(container, GTK_GRID(click_timer_grid));
-  gtk_window_set_child(GTK_WINDOW(window), container);
+  gtk_box_append(GTK_BOX(click_timer_container), click_timer_grid);
+  gtk_box_append(GTK_BOX(container), click_timer_container);
+  /*  */
+
+  /* MACRO LOADER
+   * 
+   *    Macro Editor (i)
+   * [     MACRO NAME    ]
+   * [ IMPORT ] [ CREATE ]
+   * 
+   */
+  GtkWidget* macro_loader_container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+  gtk_widget_add_css_class(macro_loader_container, "macro-loader-box");
+  gtk_widget_set_overflow(macro_loader_container, GTK_OVERFLOW_HIDDEN); // make corners round
+
+  GtkBuilder* macro_loader_builder = gtk_builder_new_from_resource("/com/the-clicker/graphics/ui/macro_loader.ui");
+  MacroEditorState* ME_STATE = g_new0(MacroEditorState, 1); // deliberatly leaked, actual pattern for GTK+
+  ME_STATE->app = app;
+  ME_STATE->main_window = window;
+
+  /*
+   * Label and info button grid
+   */
+   GtkWidget* macro_loader_label_and_info_grid = gtk_grid_new();
+   gtk_widget_set_halign(macro_loader_label_and_info_grid, GTK_ALIGN_CENTER); // set the grid in the center of the box
+   gtk_widget_set_valign(macro_loader_label_and_info_grid, GTK_ALIGN_CENTER); // set the grid in the center of the box
+   
+   GtkWidget* macro_loader_label = gtk_label_new("Macro Editor");
+   gtk_widget_add_css_class(macro_loader_label, "click-timer-label"); // reuse CSS
+   
+   GtkWidget* macro_loader_info_button = gtk_button_new();
+   
+   g_signal_connect(macro_loader_info_button, "clicked", G_CALLBACK(macroLoaderInfoDialog), ME_STATE);
+   gtk_widget_add_css_class(macro_loader_info_button, "click-timer-info-button"); // reuse CSS
+   gtk_widget_set_cursor_from_name(macro_loader_info_button, "pointer"); // make the cursor a pointer on hover
+
+   /*
+    * This doesn't actually dupe memory in any meaningful way or cause
+    * perfomance bottlenecks, its just a GTK refcounting issue about double-childing
+    */
+   GtkWidget* info_icon2 = gtk_image_new_from_resource("/com/the-clicker/graphics/assets/info_icon.png");
+   gtk_image_set_pixel_size(GTK_IMAGE(info_icon2), 18);
+   gtk_button_set_child(GTK_BUTTON(macro_loader_info_button), info_icon2); // add the icon to the button
+ 
+   gtk_grid_attach(GTK_GRID(macro_loader_label_and_info_grid), macro_loader_label, 0, 0, 1, 1);
+   gtk_grid_attach(GTK_GRID(macro_loader_label_and_info_grid), gtk_label_new(" "), 1, 0, 1, 1); // spacer
+   gtk_grid_attach(GTK_GRID(macro_loader_label_and_info_grid), macro_loader_info_button, 2, 0, 1, 1);
+   gtk_box_append(GTK_BOX(macro_loader_container), macro_loader_label_and_info_grid);
+  
+  //gtk_box_append(macro_loader_container, macro_loader_grid);
+  gtk_box_append(GTK_BOX(container), macro_loader_container);
   /*  */
 
   /* STYLE */
@@ -92,6 +144,7 @@ static void activate(GtkApplication* app, gpointer user_data) {
   g_object_unref(provider);
   /* */
   
+  gtk_window_set_child(GTK_WINDOW(window), container);
   gtk_window_present(GTK_WINDOW(window));
   
   g_object_unref(main_window_builder); // were done with builders, its been loaded into GTK's heap
