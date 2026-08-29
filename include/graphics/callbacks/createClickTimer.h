234f33ce0f1e0bcb2d6b23285201936a4d70fbce
zeviberlin <zeviberlin@gmail.com>
2026-08-29 18:33:24 -0400

    I dont have to call it that weird name anymore because its been encapsulated

--- include/graphics/callbacks/createClickTimer.h
+++ include/graphics/callbacks/createClickTimer.h
@@ -24,11 +24,11 @@ static void createClickTimer(GtkApplication* app, GtkWidget* window, GtkWidget* 
   gtk_widget_set_overflow(click_timer_container, GTK_OVERFLOW_HIDDEN); // make corners round
   
   GtkBuilder* click_timer_builder = gtk_builder_new_from_resource("/com/the-clicker/graphics/ui/click_timer.ui");
-  ClickTimerState* CT_STATE = g_new0(ClickTimerState, 1); // deliberatly leaked, actual pattern for GTK+
-  CT_STATE->app = app;
-  CT_STATE->main_window = window;
-  CT_STATE->timer_active = false;
-  CT_STATE->click_func = leftClick;
+  ClickTimerState* state = g_new0(ClickTimerState, 1); // deliberatly leaked, actual pattern for GTK+
+  state->app = app;
+  state->main_window = window;
+  state->timer_active = false;
+  state->click_func = leftClick;
 
    /*
     * Label and info button grid
@@ -36,31 +36,31 @@ static void createClickTimer(GtkApplication* app, GtkWidget* window, GtkWidget* 
     GtkWidget* label_and_info_grid = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "label_and_info_grid"));
     
     GtkWidget* click_timer_info_button = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "click_timer_info_button"));
-    g_signal_connect(click_timer_info_button, "clicked", G_CALLBACK(clickTimerInfoDialog), CT_STATE);
+    g_signal_connect(click_timer_info_button, "clicked", G_CALLBACK(clickTimerInfoDialog), state);
     gtk_widget_set_cursor_from_name(click_timer_info_button, "pointer"); // make the cursor a pointer on hover
   
     gtk_box_append(GTK_BOX(click_timer_container), label_and_info_grid);
 
   GtkWidget* click_timer_grid = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "click_timer_grid"));
   
-  CT_STATE->mins_spinbtn = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "mins_spinbtn"));
-  CT_STATE->secs_spinbtn = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "secs_spinbtn"));
-  CT_STATE->ms_spinbtn = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "ms_spinbtn"));
+  state->mins_spinbtn = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "mins_spinbtn"));
+  state->secs_spinbtn = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "secs_spinbtn"));
+  state->ms_spinbtn = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "ms_spinbtn"));
   
-  CT_STATE->x_coord_entry = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "x_coord_entry"));
-  g_signal_connect(gtk_editable_get_delegate(GTK_EDITABLE(CT_STATE->x_coord_entry)), "insert-text", G_CALLBACK(numberOnlyEntry), NULL);
-  CT_STATE->y_coord_entry = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "y_coord_entry"));
-  g_signal_connect(gtk_editable_get_delegate(GTK_EDITABLE(CT_STATE->y_coord_entry)), "insert-text", G_CALLBACK(numberOnlyEntry), NULL);
+  state->x_coord_entry = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "x_coord_entry"));
+  g_signal_connect(gtk_editable_get_delegate(GTK_EDITABLE(state->x_coord_entry)), "insert-text", G_CALLBACK(numberOnlyEntry), NULL);
+  state->y_coord_entry = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "y_coord_entry"));
+  g_signal_connect(gtk_editable_get_delegate(GTK_EDITABLE(state->y_coord_entry)), "insert-text", G_CALLBACK(numberOnlyEntry), NULL);
 
-  CT_STATE->click_func_dropdown = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "click_func_dropdown"));
+  state->click_func_dropdown = GTK_WIDGET(gtk_builder_get_object(click_timer_builder, "click_func_dropdown"));
 
   //
 
-  CT_STATE->start_stop_button = gtk_button_new_with_label("START");
-  gtk_widget_add_css_class(GTK_WIDGET(CT_STATE->start_stop_button), "start-stop-button");
-  gtk_widget_set_cursor_from_name(CT_STATE->start_stop_button, "pointer"); // make the cursor a pointer on hover
-  g_signal_connect(CT_STATE->start_stop_button, "clicked", G_CALLBACK(toggleClickTimer), CT_STATE);
-  gtk_grid_attach(GTK_GRID(click_timer_grid), CT_STATE->start_stop_button, 0, 2, 3, 1);
+  state->start_stop_button = gtk_button_new_with_label("START");
+  gtk_widget_add_css_class(GTK_WIDGET(state->start_stop_button), "start-stop-button");
+  gtk_widget_set_cursor_from_name(state->start_stop_button, "pointer"); // make the cursor a pointer on hover
+  g_signal_connect(state->start_stop_button, "clicked", G_CALLBACK(toggleClickTimer), state);
+  gtk_grid_attach(GTK_GRID(click_timer_grid), state->start_stop_button, 0, 2, 3, 1);
 
   gtk_box_append(GTK_BOX(click_timer_container), click_timer_grid);
   gtk_box_append(GTK_BOX(container), click_timer_container);
