241a3119a0dfdf21523b806f4c07598c6d00d3dc
zeviberlin <zeviberlin@gmail.com>
2026-09-20 17:02:15 -0400

    adding prompted notifications

--- include/graphics/sendNotification.h
+++ include/graphics/sendNotification.h
@@ -5,4 +5,12 @@
  * @param body      The main text content.
  * @param user_data Only for the non-macos callers to include the GTK Application
  */
-void send_notification(const char *title, const char *body, void* user_data);
+void send_notification(const char* title, const char* body, void* user_data);
+
+/*
+ * Sends a system notification with buttons
+ * 
+ * @param button1   The text in the first button - if this is clicked the function returns TRUE
+ * @param button2   The text in the second button - if this is clicked the function returns FALSE
+ */
+void send_notification_with_buttons(const char* title, const char* body, const char* button1, const char* button2, void* user_data);
