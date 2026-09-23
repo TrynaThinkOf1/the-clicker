ed5ed1d1b00010b576c60e4597faaccde2a53f29
zeviberlin <zeviberlin@gmail.com>
2026-09-23 19:50:22 -0400

    closing now frees the macro

--- include/graphics/callbacks/createEditorWindow.h
+++ include/graphics/callbacks/createEditorWindow.h
@@ -5,6 +5,7 @@
 
 #include "graphics/callbacks/unsavedWorkWarning.h"
 #include "graphics/state.h"
+#include "macros.h"
 
 static gboolean editor_window_close_request(GtkWindow* self, gpointer user_data) {
   MacroEditorState* state = (MacroEditorState*)user_data;
@@ -19,15 +20,18 @@ static gboolean editor_window_close_request(GtkWindow* self, gpointer user_data)
           return true; // they do not want to delete unsaved work
         case 2:
           gtk_window_destroy(state->editor_window);
+          freeMacro(state->mac);
           state->close_editor_window_with_unsaved_work = 0;
           break; // they want to delete unsaved work
       }
     }
   } else {
+    // simply free the macro, its already been exported
+    freeMacro(state->mac);
+    // no need to deal with state->macro_name_easy because it pointed to state->mac->name, which was just freed
+    
     gtk_window_destroy(state->editor_window);
   }
-
-  // TODO: Add freeing the macro as a mechanic
 
   return true;
 }
